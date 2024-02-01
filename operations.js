9419a705dd8fb59153fbf3c5b25aada68b35fac3
Brad W <bwittenberg1@gmail.com>
2024-01-31 20:40:59 -0800

    mathing

--- operations.js
+++ operations.js
@@ -3,3 +3,16 @@ console.log(10-4);
 console.log(10*69);
 console.log(69/15)
 console.log(69%15)
+
+console.log((10+3-5) * 10);
+
+let result = (10+3-5) * 10
+result = 10 *4;
+result++ // result = result + 1
+result-- //result = result - 1
+
+result += 5; //result = result + 5
+result -= 5; //result = result - 5
+result /= 5; //result = result / 5
+result *= 5; //result = result * 5
+console.log(result)
