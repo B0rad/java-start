aaf7b283b439f30cbf3635671e05fe9437fc2e43
Brad W <bwittenberg1@gmail.com>
2024-01-31 21:11:41 -0800

    module done

--- operations.js
+++ operations.js
@@ -1,18 +1,29 @@
-console.log(69+69);
-console.log(10-4);
-console.log(10*69);
-console.log(69/15)
-console.log(69%15)
+console.log(69 + 69);
+console.log(10 - 4);
+console.log(10 * 69);
+console.log(69 / 15);
+console.log(69 % 15);
 
-console.log((10+3-5) * 10);
+console.log((10 + 3 - 5) * 10);
 
-let result = (10+3-5) * 10
-result = 10 *4;
-result++ // result = result + 1
-result-- //result = result - 1
+let result = (10 + 3 - 5) * 10;
+result = 10 * 4;
+result++; // result = result + 1
+result--; //result = result - 1
 
 result += 5; //result = result + 5
 result -= 5; //result = result - 5
 result /= 5; //result = result / 5
 result *= 5; //result = result * 5
-console.log(result)
+console.log(result);
+
+console.log("Beese" + "churger");
+console.log("Brad" + " " + "Beesechurger");
+
+let userName = "Beesechurger";
+console.log(userName.length);
+
+
+let hobbies = ['beese', 'churger'];
+console.log(hobbies.length)
+console.log(hobbies.reverse)
